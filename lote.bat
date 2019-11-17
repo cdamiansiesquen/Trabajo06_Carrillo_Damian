2452eee0f47ff63ef3d6d5cce8d06832a0993732
cdamiansiesquen <57013170+cdamiansiesquen@users.noreply.github.com>
2019-11-17 16:05:43 -0500

    Add files via upload

---
+++ lote.bat
@@ -0,0 +1,119 @@
+python Damian/dobles/ejercicio01.py 18 120 40
+python Damian/dobles/ejercicio01.py 17 45 35
+python Damian/dobles/ejercicio01.py 80 14 65
+
+python Damian/dobles/ejercicio02.py 120 100 130
+python Damian/dobles/ejercicio02.py 130 100 50
+python Damian/dobles/ejercicio02.py 200 100 80
+
+python Damian/dobles/ejercicio03.py 45 25 30
+python Damian/dobles/ejercicio03.py 25 25 25
+python Damian/dobles/ejercicio03.py 40 10 19
+
+python Damian/dobles/ejercicio04.py 15 15 19
+python Damian/dobles/ejercicio04.py 15 18 17
+python Damian/dobles/ejercicio04.py 18 19 23
+
+python Damian/dobles/ejercicio05.py 15 12 30
+python Damian/dobles/ejercicio05.py 17 15 42
+python Damian/dobles/ejercicio05.py 20 15 45
+
+python Damian/dobles/ejercicio06.py "juan" 12 17 58
+python Damian/dobles/ejercicio06.py "Andre" 10 75 38
+python Damian/dobles/ejercicio06.py "Alejandra" 100 75 45
+
+python Damian/dobles/ejercicio07.py "juan" 45 35 48
+python Damian/dobles/ejercicio07.py "juan" 75 45 65
+python Damian/dobles/ejercicio07.py "juan" 80 45 64
+
+python Damian/dobles/ejercicio08.py "Juan" 18 18 24
+python Damian/dobles/ejercicio08.py "Juan" 15 24 36
+python Damian/dobles/ejercicio08.py "Juan" 15 15 15
+
+
+
+
+
+python Damian/dobles/ejercicio10.py 12 7 2
+python Damian/dobles/ejercicio10.py 11 7 1
+python Damian/dobles/ejercicio10.py 7 12 5
+
+python Damian/simples/ejercicio11.py 25 17 31
+python Damian/simples/ejercicio11.py 14 15 35
+python Damian/simples/ejercicio11.py 14 25 35
+
+python Damian/simples/ejercicio12.py 250 240 150
+python Damian/simples/ejercicio12.py 250 140 170
+python Damian/simples/ejercicio12.py 280 120 253
+
+>python Damian/simples/ejercicio13.py 15 10 90
+python Damian/simples/ejercicio13.py 15 15 120
+python Damian/simples/ejercicio13.py 30 15 120
+
+python Damian/simples/ejercicio14.py 50 25 35
+python Damian/simples/ejercicio14.py 54 26 35
+python Damian/simples/ejercicio14.py 54 30 45
+
+python Damian/simples/ejercicio15.py 120 75 35
+python Damian/simples/ejercicio15.py 100 75 85
+python Damian/simples/ejercicio15.py 120 45 65
+
+python Damian/simples/ejercicio16.py 10 5 8
+python Damian/simples/ejercicio16.py 11 4 3
+python Damian/simples/ejercicio16.py 13 8 4
+
+python Damian/simples/ejercicio17.py 5 5 6
+python Damian/simples/ejercicio17.py 5 7 9
+python Damian/simples/ejercicio17.py 5 5 4
+
+python Damian/simples/ejercicio18.py 75 85 70
+python Damian/simples/ejercicio18.py 75 90 100
+python Damian/simples/ejercicio18.py 85 75 45
+
+python Damian/simples/ejercicio19.py 25 15 8
+python Damian/simples/ejercicio19.py 25 20 8
+python Damian/simples/ejercicio19.py 28 17 15
+
+python Damian/simples/ejercicio20.py 35 85 15
+python Damian/simples/ejercicio20.py 35 100 25
+python Damian/simples/ejercicio20.py 35 80 45
+
+python Damian/multiples/ejercicio21.py "luis" 400 250 150
+python Damian/multiples/ejercicio21.py "luis" 405 100 120
+python Damian/multiples/ejercicio21.py "luis" 405 120 150
+
+python Damian/multiples/ejercicio22.py 15 45 25
+python Damian/multiples/ejercicio22.py 15 40 15
+python Damian/multiples/ejercicio22.py 15 30 45
+
+python Damian/multiples/ejercicio23.py 25 15 35
+python Damian/multiples/ejercicio23.py 25 40 25
+python Damian/multiples/ejercicio23.py 25 40 40
+
+python Damian/multiples/ejercicio24.py 120 150 48
+python Damian/multiples/ejercicio24.py 120 175 85
+python Damian/multiples/ejercicio24.py 120 150 145
+
+python Damian/multiples/ejercicio25.py 25 45 75
+python Damian/multiples/ejercicio25.py 25 75 45
+python Damian/multiples/ejercicio25.py 25 80 45
+
+python Damian/multiples/ejercicio26.py 60 25 48
+python Damian/multiples/ejercicio26.py 45 25 85
+python Damian/multiples/ejercicio26.py 25 45 15
+
+python Damian/multiples/ejercicio27.py 20 20 20
+python Damian/multiples/ejercicio27.py 15 14 20
+python Damian/multiples/ejercicio27.py 15 20 20
+
+python Damian/multiples/ejercicio28.py 15 5 8
+python Damian/multiples/ejercicio28.py 15 10 8
+python Damian/multiples/ejercicio28.py 10 5 5
+
+python Damian/multiples/ejercicio29.py "antony" 45 55 20
+python Damian/multiples/ejercicio29.py "antony" 45 45 20
+python Damian/multiples/ejercicio29.py "antony" 45 50 14
+
+python Damian/multiples/ejercicio30.py " Karen" 8 10 12
+python Damian/multiples/ejercicio30.py " Karen" 8 8 5
+python Damian/multiples/ejercicio30.py " Karen" 8 8 10
